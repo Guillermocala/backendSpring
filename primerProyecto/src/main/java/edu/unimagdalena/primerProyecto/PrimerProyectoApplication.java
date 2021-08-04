1e863be431609e826b490a8fab61ce34fd61a411
guillermo cala <guillermo.cala7@gmail.com>
2021-08-03 23:56:36 -0500

    first commit

---
+++ primerProyecto/src/main/java/edu/unimagdalena/primerProyecto/PrimerProyectoApplication.java
@@ -0,0 +1,13 @@
+package edu.unimagdalena.primerProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+	}
+
+}
---
+++ primerProyecto/src/main/java/edu/unimagdalena/primerProyecto/model/Mascota.java
@@ -0,0 +1,72 @@
+package edu.unimagdalena.primerProyecto.model;
+
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="Mascotas")
+public class Mascota {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String raza;
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date fechaNacimiento;
+
+	public Mascota() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	
+	
+}
